06a52334a0ff7458e896d528cee341950094918c
hemantksingh <hemantksingh.hk@gmail.com>
2016-06-15 00:58:41 +0100

    Hemant #266 Create application plus environment.

--- src/gulpfile.babel.js
+++ src/gulpfile.babel.js
@@ -17,8 +17,52 @@ gulp.task('deploy', () => {
         apiVersion: '2010-12-01'
     });
 
+    let appName = "smbc-app",
+        appVersionLabel = "smbc-app-1",
+        environmentName = "smbc-env-1";
+
     eb.checkDNSAvailability({
-        CNAMEPrefix: "test-app-1"
-      }, handleResponse
+        CNAMEPrefix: appName
+      }, (err, data) => {
+        if (err) console.log(err, err.stack);
+        else {
+          console.log("Creating application..");
+          eb.createApplication({
+              ApplicationName: appName,
+              Description: "Test application"
+            }, (err, data) => {
+              if (err) console.log(err, err.stack);
+              else {
+                  console.log("Creating application version..");
+                  eb.createApplicationVersion({
+                      ApplicationName: appName,
+                      VersionLabel: appVersionLabel,
+                      AutoCreateApplication: false,
+                      Description: "Test application",
+                      Process: true,
+                      SourceBundle: {
+                        S3Bucket: "elasticbeanstalk-us-west-2-514467551670",
+                        S3Key: "201616526M-stockport-web-source.zip"
+                      }
+                    }, (err, data) => {
+                      if (err) console.log(err, err.stack);
+                      else {
+                        console.log("Creating environment..");
+                        eb.createEnvironment({
+                            ApplicationName: appName,
+                            CNAMEPrefix: appName,
+                            EnvironmentName: environmentName,
+                            SolutionStackName: "64bit Amazon Linux 2016.03 v2.1.1 running Multi-container Docker 1.9.1 (Generic)",
+                            VersionLabel: appVersionLabel
+                          }, handleResponse
+                        );
+                      }
+                    }
+                  );
+              }
+            }
+          );
+        }
+      }
     );
 });
